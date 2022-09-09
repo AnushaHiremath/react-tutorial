b95f0a5c2b830187db576352276a35bb4f75d70f
AnushaHiremath <hiremath.anusha@gmail.com>
2022-09-09 20:37:50 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { Button } from "@mui/material";
 import React from "react";
+import SideBar from "./components/SideBar";
+import NewsFeed from "./components/NewsFeed";
+import NavBar from "./components/NavBar";
+import { Box, Container, Stack } from "@mui/system";
+import RightBar from "./components/RightBar";
 
 function App() {
   return (
-    <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color="secondary" size="small">
-        Contained
-      </Button>
-      <Button variant="outlined" disableds>
-        Outlined
-      </Button>
-    </div>
+    <Box>
+      {/* navbar */}
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <SideBar />
+        <NewsFeed />
+        <RightBar />
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/components/NewsFeed.js
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const NewsFeed = () => {
+  return (
+    <Box bgcolor="pink" flex={4} p={2}>
+      NewsFeed
+    </Box>
+  );
+};
+
+export default NewsFeed;
